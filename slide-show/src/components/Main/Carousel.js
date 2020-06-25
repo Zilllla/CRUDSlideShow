afff42282c2b3f48ffe62298018701a9da63d617
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-06-25 02:32:00 +0100

    nped react elastic carousel and may regret it. only time will tell

--- slide-show/src/components/Main/Carousel.js
+++ slide-show/src/components/Main/Carousel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Elastic from "react-elastic-carousel";
 
 //BASE URL!!!!
 const baseURL = "http://localhost:4000/";
@@ -29,16 +30,18 @@ class Carousel extends React.Component {
     return (
       <div>
         <h1>Testing</h1>
-        {this.state.imgSet.map(image => (
-          <div>
-            <h3>{image.title}</h3>
-            <img src={image.images.img1} />
-            <img src={image.images.img2} />
-            <img src={image.images.img3} />
-            <img src={image.images.img4} />
-            <img src={image.images.img5} />
-          </div>
-        ))}
+        <Elastic>
+          {this.state.imgSet.map(image => (
+            <div>
+              <h3>{image.title}</h3>
+              <img src={image.images.img1} />
+              <img src={image.images.img2} />
+              <img src={image.images.img3} />
+              <img src={image.images.img4} />
+              <img src={image.images.img5} />
+            </div>
+          ))}
+        </Elastic>
       </div>
     );
   }
